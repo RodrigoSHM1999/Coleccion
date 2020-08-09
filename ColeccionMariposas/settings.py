c27abe40faab8d46fa8d9eafb542b294df442892
RodrigoSHM1999 <rhumanma@unsa.edu.pe>
2020-08-09 00:50:17 -0500

    elimine

--- ColeccionMariposas/settings.py
+++ ColeccionMariposas/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'ColeccionMariposas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE' : 'django.db.backends.postgresql',
+        'NAME' : 'coleccion_de_mariposas',
+        'USER' : 'postgres',
+        'PASSWORD' : '1234',
+        'HOST' : 'localhost'
     }
 }
 
---
+++ colecc/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ColeccConfig(AppConfig):
+    name = 'colecc'
